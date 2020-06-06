16e95dd2933fe22c46ffa03d8148a6bbf59a27b1
iamHolo <nneketaa@yandex.ru>
2020-06-06 20:44:47 +0500

    fin

---
+++ Book2/Book2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Book2
+{
+
+    class Book
+    {
+        private int year;
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                if (value > 2020)
+                    year = 2020;
+                else if (value < 1940)
+                    year = 1940;
+                else year = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Year = 2045;
+            Console.WriteLine(book.Year + " (В коде указан год >2020)");
+            book.Year = 1945;
+            Console.WriteLine(book.Year);
+            Console.ReadLine();
+         }
+    }
+}
